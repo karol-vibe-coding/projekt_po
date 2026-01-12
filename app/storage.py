74d90b9c4712b0f3ef7c6ce2b880fe90c7ada917
Karol <105021884+karol-vibe-coding@users.noreply.github.com>
2026-01-12 10:48:44 +0100

    Add get_all method to UserStorage

--- app/storage.py
+++ app/storage.py
@@ -1,7 +1,13 @@
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 
 class UserStorage:
     def __init__(self):
         self._users: Dict[int, dict] = {}
         self._next_id: int = 1
+    
+    def get_all(self) -> List[dict]:
+        return [
+            {"id": user_id, "name": data["name"], "lastname": data["lastname"]}
+            for user_id, data in self._users.items()
+        ]
